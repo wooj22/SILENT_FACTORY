460007c6230275fd4931b5f51e8fb0ade5149524
woojeong <77192050+wooj22@users.noreply.github.com>
2024-12-17 00:34:13 +0900

    Fix | 발소리 무한루프

--- Assets/02. Scripts/MainGame/Player/FootStep.cs
+++ Assets/02. Scripts/MainGame/Player/FootStep.cs
@@ -4,44 +4,45 @@ using UnityEngine;
 
 public class FootStep : MonoBehaviour
 {
-    [SerializeField] Player player;
-    AudioSource audioSource;
+    [SerializeField] private Player player;
+    private AudioSource audioSource;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        //StartCoroutine(FootSpet());
+        StartCoroutine(FootStepCoroutine());
     }
 
-    IEnumerator FootSpet()
+    IEnumerator FootStepCoroutine()
     {
-        while (!player.isDie)
+        while (!player.isDie) // 플레이어가 죽지 않았을 때 반복
         {
             switch (player.currentMovementState)
             {
                 case Player.MovementState.Walking:
-                    //audioSource.Stop();
                     audioSource.Play();
                     yield return new WaitForSeconds(0.5f);
                     break;
+
                 case Player.MovementState.Running:
-                    //audioSource.Stop();
                     audioSource.Play();
                     yield return new WaitForSeconds(0.3f);
                     break;
+
                 case Player.MovementState.Sneaking:
-                    //audioSource.Stop();
                     audioSource.Play();
                     yield return new WaitForSeconds(1f);
                     break;
+
                 case Player.MovementState.Idle:
                     audioSource.Stop();
+                    yield return null;
                     break;
+
                 default:
+                    yield return null;
                     break;
             }
         }
-        
-        yield return null;
     }
 }
